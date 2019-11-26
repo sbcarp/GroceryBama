029f84d976b92e86dc3cbafdb28c848ae65174e7
Han Fang <hfang8@crimson.ua.edu>
2019-11-25 21:28:16 -0600

    Add payment method entity Change homepage checkboxes to radio buttons because it's little complicated to implement mutiple groups in SQL and it's not within the project requirement

---
+++ GroceryBama/Entities/PaymentMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace GroceryBama.Entities
+{
+    [DataContract]
+    public class PaymentMethod
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AccountNumber { get; set; }
+        public string RoutineNumber { get; set; }
+    }
+}
